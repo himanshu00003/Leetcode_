6afd9e725cf20c3fd81c742ed4b2e29539e2107b
himanshu00003 <102656780+himanshu00003@users.noreply.github.com>
2024-07-07 11:35:56 +0530

    283. Move Zeroes.java

---
+++ (023) 283. Move Zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int temp;
+        int a = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                nums[a++] = nums[i];
+            }
+        }
+
+        while (a < n) {
+            nums[a++] = 0;
+        }
+    }
+}
