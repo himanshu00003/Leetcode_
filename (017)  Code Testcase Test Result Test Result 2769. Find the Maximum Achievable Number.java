3a45ea13b7ab5f08462260f2a049586bd73476bc
himanshu00003 <102656780+himanshu00003@users.noreply.github.com>
2024-07-07 11:24:40 +0530

    2769. Find the Maximum Achievable Number.java

---
+++ (017)  Code Testcase Test Result Test Result 2769. Find the Maximum Achievable Number.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int theMaximumAchievableX(int num, int t) {
+        //o/p - t = num + t
+        //op = num + t + t
+        num = num + t + t;
+        return num;
+    }
+}
