5e94875d5c1cce0b378b149f20dee5a3a644153d
himanshu00003 <102656780+himanshu00003@users.noreply.github.com>
2024-07-07 11:26:49 +0530

    2011. Final Value of Variable After Performing Operations.java

---
+++ (018) 2011. Final Value of Variable After Performing Operations.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        if (operations.length == 3) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
