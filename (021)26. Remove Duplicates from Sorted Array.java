52a7380e6f0fd9a075522f0362a7c96c3e1d2809
himanshu00003 <102656780+himanshu00003@users.noreply.github.com>
2024-07-07 11:48:08 +0530

    26. Remove Duplicates from Sorted Array.java

--- (021)26. Remove Duplicates from Sorted Array.java
+++ (021)26. Remove Duplicates from Sorted Array.java
@@ -1,3 +1,27 @@
+//Solution 1
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        
+        int a = 1; // Initialize count of unique elements, starting with the first element
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[a++] = nums[i]; // Move unique element to the correct position
+            }
+        }
+        
+        return a; // Return the count of unique elements
+    }
+}
+
+
+
+
+//Solution 2
 class Solution {
     public int removeDuplicates(int[] nums) {
         if (nums.length == 0) {
