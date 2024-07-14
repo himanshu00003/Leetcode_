3f03fd81675e58fc9f018cb1d49bc4101d2a0347
Himanshu Mehta <102656780+himanshu00003@users.noreply.github.com>
2024-07-14 15:33:37 +0530

    100352. Lexicographically Smallest String After a Swap.java

--- (045) 100352. Lexicographically Smallest String After a Swap.java
+++ (045) 100352. Lexicographically Smallest String After a Swap.java
@@ -1,3 +1,4 @@
+//SOLUTION 1 DONE BY CONVERSION (CHAR ARRAY)
 class Solution {
     public String getSmallestString(String s) {
         char  chars[] = s.toCharArray();
@@ -5,7 +6,7 @@ class Solution {
         int n = chars.length;
     
         for(int i = 0 ; i < n - 1; i++){
-            if(chars[i] > chars[i+1] && sameparity(chars[i] , chars[i+1])){
+            if((chars[i] % 2 == chars[i+1] % 2) && (chars[i] > chars[i+1])){
                     temp = chars[i];
                     chars[i] = chars[i+1];
                     chars[i+1] = temp;
@@ -14,11 +15,54 @@ class Solution {
         }
         return new String(chars);
     }
+}
 
+//SOLUTION DONE BY STRINGBUILDER
 
+public class Solution {
+    public String getSmallestString(String s) {
+        StringBuilder sb = new StringBuilder(s);
 
+        for (int i = 0; i < sb.length() - 1; i++) {
+            if ((sb.charAt(i) % 2 == sb.charAt(i + 1) % 2) && (sb.charAt(i) > sb.charAt(i + 1))) {
+                char temp = sb.charAt(i);
+                sb.setCharAt(i, sb.charAt(i + 1));
+                sb.setCharAt(i + 1, temp);
+                break;
+            }
+        }
 
-    private boolean sameparity(char a  , char b){
-        return  (a - '0') % 2 == (b - '0') % 2;
+        return sb.toString();
     }
 }
+
+
+
+
+
+
+//SOLUTION 2 ALSO DONE BY CONVERSION (TO CHAR ARRAY BUT WITH THE HELP OF FUNCTION)
+// class Solution {
+//     public String getSmallestString(String s) {
+//         char  chars[] = s.toCharArray();
+//         char temp;
+//         int n = chars.length;
+    
+//         for(int i = 0 ; i < n - 1; i++){
+//             if(chars[i] > chars[i+1] && sameparity(chars[i] , chars[i+1])){
+//                     temp = chars[i];
+//                     chars[i] = chars[i+1];
+//                     chars[i+1] = temp;
+//                     break;
+//             }
+//         }
+//         return new String(chars);
+//     }
+
+
+
+
+//     private boolean sameparity(char a  , char b){
+//         return  (a - '0') % 2 == (b - '0') % 2;
+//     }
+// }
