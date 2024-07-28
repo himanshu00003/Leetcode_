c33317e0b21bd8f2b65fd35e8d87cd4b0e36f6d9
Himanshu Mehta <102656780+himanshu00003@users.noreply.github.com>
2024-07-28 22:42:46 +0530

    1. Two Sum.java

--- (001)1. Two Sum.java
+++ (001)1. Two Sum.java
@@ -1,3 +1,20 @@
+//1ST WAY USING HASHMAPS
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+         HashMap<Integer , Integer> map = new HashMap<>();
+         for(int i = 0 ; i < nums.length ; i++){
+            int diff = (target - nums[i]);
+            if(map.containsKey(diff)){
+                return new int[]{map.get(diff) , i};
+            }
+            map.put(nums[i] ,i);
+         }
+        return null;
+    }
+}
+
+
+// 2ND  WAY USING 2 POINTERS
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int arr[] = new int[2];
