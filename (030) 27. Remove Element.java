ff713bef85af4b79b7587708ec4860bee5d153b4
Himanshu Mehta <102656780+himanshu00003@users.noreply.github.com>
2025-05-26 22:34:48 +0530

    (030) 27. Remove Element.java

--- (030) 27. Remove Element.java
+++ (030) 27. Remove Element.java
@@ -1,3 +1,4 @@
+// Method 1
 class Solution {
     public int removeElement(int[] nums, int val) {
         ArrayList<Integer> arr = new ArrayList<>();
@@ -11,5 +12,21 @@ class Solution {
             nums[i] = arr.get(i);
         }
         return arr.size();
+    }
+}
+
+// Method 2
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0; // Pointer for position to overwrite
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
     }
 }
